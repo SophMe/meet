fa48c1fabd076043cb8f82d040cd562a94390050
SophMe <116891325+SophMe@users.noreply.github.com>
2023-03-20 14:13:30 +0100

    create api.js to get list of event locations

---
+++ src/api.js
@@ -0,0 +1,7 @@
+// make event locations availabe to CitySearch
+
+export const extractLocations = (events) => {
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
+};
