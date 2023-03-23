7621b5407ec2b7a54e3c285d9f1ba7f290b23454
SophMe <116891325+SophMe@users.noreply.github.com>
2023-03-23 18:58:20 +0100

    add getEvents

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
-// make event locations availabe to CitySearch
+// This file makes event locations availabe to CitySearch
+
+import { mockData } from "./mock-data";
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
+};
+
+export const getEvents = async () => {
+  return mockData;
 };
