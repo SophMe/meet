0ed96d10f7a7c45e814ffaa00f944bacf6bea64b
SophMe <116891325+SophMe@users.noreply.github.com>
2023-04-11 11:35:03 +0200

    set up Atatus

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
+atatus.config('0a47bcac788349ec82621bc2f0b3015a').install();
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 const root = document.getElementById('root');
 render(
@@ -24,3 +26,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// atatus.notify(new Error('Test Atatus Setup'));
