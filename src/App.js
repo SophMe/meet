be0c3f8ebecfdb59f41481871a75e2c8adbcaea6
SophMe <116891325+SophMe@users.noreply.github.com>
2023-05-04 19:25:12 +0200

    add  chart with rechart

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { extractLocations, getEvents, getAccessToken, checkToken } from './api';
 import './nprogress.css';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import { ScatterChart, Scatter, XAxis,YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -64,17 +65,44 @@ updateEventCount = (event) => {
   });
 }
 
+// get cities and number of events for chart
+getData = () => {
+  const {locations, events} = this.state;
+  const data = locations.map((location)=>{
+    const number = events.filter((event) => event.location === location).length
+    const city = location.split(', ').shift()
+    return {city, number};
+  })
+  return data;
+};
+
   render() {
     if (this.state.showWelcomeScreen === undefined) return <div className="app" />
-    const {events} = this.state;
-    console.log(events);
+    const { locations, eventCount, events} = this.state;
     return (
       <div className="app-container">
         <div className="app">
+          <h2>Meet App</h2>
+          <h3>Choose your nearest city</h3>
           <WarningAlert text={this.state.warningText} />
-          <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-          <NumberOfEvents eventCount={this.state.eventCount} updateEventCount={this.updateEventCount} />
-          <EventList events={this.state.events} />    {/* pass the state to EventList as a prop of events */}
+          <CitySearch locations={locations} updateEvents={this.updateEvents} />
+          <NumberOfEvents updateEventCount={this.updateEventCount} eventCount={eventCount}
+          />
+          <h3>Events in each City</h3>
+            <div className='data-vis-wrapper'>
+              <ResponsiveContainer height={400}>
+                <ScatterChart
+                  margin={{ top: 20, right: 20, bottom: 20, left: 20, }}
+                >
+                  <CartesianGrid />
+                  <XAxis dataKey="city" type="category" name="city" />
+                  <YAxis dataKey="number" type="number" name="number of events" allowDecimals={false} />
+                  <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                  <Scatter data={this.getData()} fill="#8884d8" />
+                </ScatterChart>
+              </ResponsiveContainer>
+            </div>
+          <EventList events={events} />    {/* pass the state to EventList as a prop of events */}
           <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => {getAccessToken()}} />
         </div>
       </div>
